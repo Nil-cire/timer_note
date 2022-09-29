847e29310bf5445e69ef6246017a905333caf175
Nilcire <alun5236@gmail.com>
2022-09-29 15:31:34 +0800

    feat: show and add note and show detail

--- lib/ui/custom/HomeNoteView.dart
+++ lib/ui/custom/HomeNoteView.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:timer_note/data/entity/NoteFileEntity.dart';
+import 'package:timer_note/data/entity/SubjectEntity.dart';
 
 import '../../value/MyDimension.dart';
 
 class HomeNoteView extends StatelessWidget {
   const HomeNoteView(this.noteFile, {Key? key}) : super(key: key);
 
-  final NoteFileEntity noteFile;
+  final SubjectEntity noteFile;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: const BorderRadius.all(
-          Radius.circular(MyDimension.mainBorderRadius)),
-      child: AspectRatio(
-        aspectRatio: 1 / 2,
-        child: Container(
-          padding: const EdgeInsets.all(MyDimension.itemMainPadding),
-          color: Colors.red,
+      borderRadius:
+          const BorderRadius.all(Radius.circular(MyDimension.mainBorderRadius)),
+      child: Container(
+        padding: const EdgeInsets.all(MyDimension.itemMainPadding),
+        color: Colors.blue,
+        child: Text(
+          noteFile.title,
+          style: const TextStyle(
+              color: Colors.white, fontSize: MyDimension.fontSizeItemTitle),
         ),
       ),
     );
--- lib/ui/note_detail/NoteDetailViewModel.dart
+++ lib/ui/note_detail/NoteDetailViewModel.dart
@@ -1,5 +1,5 @@
 import 'package:bloc/bloc.dart';
-import 'package:timer_note/data/entity/NoteEneity.dart';
+import 'package:timer_note/data/entity/NoteEntity.dart';
 
 class NoteDetailViewModel extends Cubit<NoteDetailViewModelState> {
   NoteDetailViewModel(this.note) : super(NoteDetailViewModelState.init);
